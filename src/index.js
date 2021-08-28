8c13e361207842a640ac836f9b4f62b3a01fb257
AtomBaf <github@fabiengrossetete.com>
2021-08-28 18:15:35 +0200

    Add doorbell support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var switches = require('./switch')
 var temperature = require('./temperature')
 var humidity = require('./humidity')
 var smoke = require('./smoke')
+var doorbell = require('./doorbell')
 var fan = require('./fan')
 var platform = require('./platform')
 
@@ -13,6 +14,7 @@ module.exports = (api) => {
     api.registerAccessory('mqtt-tasmota-temperature', temperature)
     api.registerAccessory('mqtt-tasmota-humidity', humidity)
     api.registerAccessory('mqtt-tasmota-smoke', smoke)
+    api.registerAccessory('mqtt-tasmota-doorbell', doorbell)
     api.registerAccessory('mqtt-tasmota-fan', fan)
 
     api.registerPlatform('mqtt-tasmota', platform)
